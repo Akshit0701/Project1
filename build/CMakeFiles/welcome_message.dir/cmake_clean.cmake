57309a1070aa22d0c4bc57e1cf13ea15ab59398c
Akshit0701 <alunia381@gmail.com>
2023-08-20 20:29:34 -0400

    udacity_robotics_software_engineer_project1

---
+++ build/CMakeFiles/welcome_message.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/welcome_message.dir/script/welcome_message.cpp.o"
+  "CMakeFiles/welcome_message.dir/script/welcome_message.cpp.o.d"
+  "libwelcome_message.pdb"
+  "libwelcome_message.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/welcome_message.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
